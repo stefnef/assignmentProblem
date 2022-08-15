5a4c68e12ed8d07812e3e52ace52c947110396fc
stefnef <steffen@vorhundert.de>
2022-08-15 20:51:44 +0200

    removed obsolet TODO

--- src/main/kotlin/de/hoersendung/steffen/assignmentProblem/domain/entity/Pupil.kt
+++ src/main/kotlin/de/hoersendung/steffen/assignmentProblem/domain/entity/Pupil.kt
@@ -3,4 +3,3 @@ package de.hoersendung.steffen.assignmentProblem.domain.entity
 import de.hoersendung.steffen.assignmentProblem.domain.valueObject.PupilName
 
 data class Pupil(val name: PupilName)
-//TODO add equals
