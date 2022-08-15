1909d9233a233ebe6c13a6da811af0b2daea636d
stefnef <steffen@vorhundert.de>
2022-08-15 20:44:53 +0200

    added missing equals and hash methods

--- src/main/kotlin/de/hoersendung/steffen/assignmentProblem/domain/entity/Subject.kt
+++ src/main/kotlin/de/hoersendung/steffen/assignmentProblem/domain/entity/Subject.kt
@@ -5,5 +5,18 @@ import de.hoersendung.steffen.assignmentProblem.domain.valueObject.SubjectName
 
 data class Subject(val name: SubjectName, val capacity: Capacity) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Subject
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
 }
-//TODO add equals for name
---
+++ src/test/kotlin/de/hoersendung/steffen/assignmentProblem/domain/entity/SubjectTest.kt
@@ -0,0 +1,31 @@
+package de.hoersendung.steffen.assignmentProblem.domain.entity
+
+import de.hoersendung.steffen.assignmentProblem.domain.valueObject.Capacity
+import de.hoersendung.steffen.assignmentProblem.domain.valueObject.SubjectName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SubjectTest {
+
+    @Test
+    internal fun `should use name as id`() {
+        val first = Subject(SubjectName("First"), Capacity(1))
+        val sameName = Subject(SubjectName("First"), Capacity(2))
+        val differentName = Subject(SubjectName("Different"), Capacity(1))
+
+        assertThat(first).isEqualTo(sameName)
+        assertThat(first).isNotEqualTo(differentName)
+        assertThat(sameName).isNotEqualTo(differentName)
+    }
+
+    @Test
+    internal fun `should use name for hash`() {
+        val first = Subject(SubjectName("First"), Capacity(1))
+        val sameName = Subject(SubjectName("First"), Capacity(2))
+        val differentName = Subject(SubjectName("Different"), Capacity(1))
+
+        assertThat(first.hashCode()).isEqualTo(sameName.hashCode())
+        assertThat(first.hashCode()).isNotEqualTo(differentName.hashCode())
+        assertThat(sameName.hashCode()).isNotEqualTo(differentName.hashCode())
+    }
+}
