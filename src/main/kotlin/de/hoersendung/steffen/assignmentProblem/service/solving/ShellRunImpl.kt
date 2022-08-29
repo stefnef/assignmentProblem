1d6d81555718c5c2599b862b8ec8f4a76f2f79e6
stefnef <steffen@vorhundert.de>
2022-08-29 20:08:51 +0200

    added more logging and changed shell runner call version 1.0.1

--- src/main/kotlin/de/hoersendung/steffen/assignmentProblem/service/solving/ShellRunImpl.kt
+++ src/main/kotlin/de/hoersendung/steffen/assignmentProblem/service/solving/ShellRunImpl.kt
@@ -2,17 +2,21 @@ package de.hoersendung.steffen.assignmentProblem.service.solving
 
 import com.lordcodes.turtle.shellRun
 import de.hoersendung.steffen.assignmentProblem.configuration.OutputConfiguration
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class ShellRunImpl(
-    private val outputConfiguration : OutputConfiguration
+    private val outputConfiguration : OutputConfiguration,
+    private val logger : Logger
 ) : ShellRun {
 
     override fun execute(): String {
         return try {
             runCommand()
         } catch (e: RuntimeException) {
+            logger.error(e.message)
             return errorMessage(e)
         }
     }
@@ -21,7 +25,12 @@ class ShellRunImpl(
         return COMMAND != ""
     }
 
-    private fun runCommand() = shellRun(COMMAND, listOf("-f", "${outputConfiguration.directory}/assignmentProblem.zpl"))
+    private fun runCommand() : String {
+        logger.info("executing '$COMMAND -f ${outputConfiguration.directory}/assignmentProblem.zpl'")
+        val rawOutput = shellRun(COMMAND, listOf("-f", "assignmentProblem.zpl"), File(outputConfiguration.directory))
+        logger.info("finished external command")
+        return rawOutput
+    }
 
     private fun errorMessage(e: RuntimeException) =
         e.message ?: "could not run command \"$COMMAND -f ${outputConfiguration.directory}/assignmentProblem.zpl\""
--- src/test/kotlin/de/hoersendung/steffen/assignmentProblem/service/solving/ShellRunImplTest.kt
+++ src/test/kotlin/de/hoersendung/steffen/assignmentProblem/service/solving/ShellRunImplTest.kt
@@ -3,20 +3,25 @@ package de.hoersendung.steffen.assignmentProblem.service.solving
 import de.hoersendung.steffen.assignmentProblem.configuration.OutputConfiguration
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.slf4j.Logger
 
 internal class ShellRunImplTest {
 
+    private val logger : Logger = mock()
     private val outputDirectory = "some/directory"
     private val outputConfiguration = OutputConfiguration(directory = outputDirectory)
 
-    private val runner = ShellRunImpl(outputConfiguration)
+    private val runner = ShellRunImpl(outputConfiguration, logger)
 
     @Test
     internal fun `should call command`() {
         ShellRunImpl.COMMAND = "ls"
 
         val rawOutput = runner.execute()
-        assertThat(rawOutput).contains("some/directory/assignmentProblem.zpl")
+        assertThat(rawOutput).contains("Running shell command failed")
+        verify(logger).info("executing 'ls -f some/directory/assignmentProblem.zpl'")
     }
 
     @Test
